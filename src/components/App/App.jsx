c5257338ad64ab3063b0525f996268ab8e9bae78
Tomás F. Vilalta <84082278+TomasVilalta@users.noreply.github.com>
2024-02-26 20:26:41 -0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "../Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div> This is were it all began! 🐉🍜 </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.jsx";
+import "./reset.css";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

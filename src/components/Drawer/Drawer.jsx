9f71ad30bd31d5439dcaad4930d0838840323ef3
Tomás F. Vilalta <84082278+TomasVilalta@users.noreply.github.com>
2024-03-13 10:26:09 -0300

    Add react-focus-lock and react-remove-scroll to Drawer for improved accessibility

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { X as Close } from "lucide-react";
-// import FocusLock from 'react-focus-lock';
-// import { RemoveScroll } from 'react-remove-scroll';
+import FocusLock from "react-focus-lock";
+import { RemoveScroll } from "react-remove-scroll";
 
 import styles from "./Drawer.module.css";
 
@@ -21,19 +21,19 @@ function Drawer({ handleDismiss, children }) {
   }, [handleDismiss]);
 
   return (
-    // <FocusLock returnFocus={true}>
-    //   <RemoveScroll>
-    <div className={styles.wrapper}>
-      <div className={styles.backdrop} onClick={handleDismiss} />
-      <div className={styles.drawer}>
-        <div>{children}</div>
-        <button className={styles.closeBtn} onClick={handleDismiss}>
-          <Close size={18} /> Dismiss
-        </button>
-      </div>
-    </div>
-    //   </RemoveScroll>
-    // </FocusLock>
+    <FocusLock returnFocus={true}>
+      <RemoveScroll>
+        <div className={styles.wrapper}>
+          <div className={styles.backdrop} onClick={handleDismiss} />
+          <div className={styles.drawer}>
+            <div>{children}</div>
+            <button className={styles.closeBtn} onClick={handleDismiss}>
+              <Close size={18} /> Dismiss
+            </button>
+          </div>
+        </div>
+      </RemoveScroll>
+    </FocusLock>
   );
 }
 
